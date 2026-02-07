8ca66e80699aca9780f9ee4141f4101b1cd40310
Karuthapandi <karuthapandikarthi21@gmail.com>
2026-02-07 17:52:46 +0530

    Delete CollegeInformation directory

--- CollegeInformation/src/College/Department.java
+++
@@ -1,6 +0,0 @@
-package College;
-public class Department extends Hostel{
-public void depName() {
-	System.out.println(" Department Name:Bachelor Of Computer Application");
-}
-}
--- CollegeInformation/src/College/Hostel.java
+++
@@ -1,6 +0,0 @@
-package College;
-public class Hostel extends Student {
-public void hostelName() {
-	System.out.println(" Hostel Name:Seva");
-}
-}
