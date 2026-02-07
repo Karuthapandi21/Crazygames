3bb88b548655c41f5e1dfb418692a9e1ee1a8f30
Karuthapandi <karuthapandikarthi21@gmail.com>
2026-02-07 17:58:29 +0530

    Delete Automation directory

--- Automation/src/test/java/org/porunai/Pass.java
+++
@@ -1,8 +0,0 @@
-package org.porunai;
-import org.testng.annotations.DataProvider;
-public class Pass  {
-@DataProvider(name="maximize")
-	public void get() {
-}
-	
-}
--- Automation/src/test/java/org/porunai/ScreenShot.java
+++
@@ -1,4 +0,0 @@
-package org.porunai;
-public class ScreenShot {
-
-}
